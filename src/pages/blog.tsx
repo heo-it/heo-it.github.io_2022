5381986f81f736d2fbfd809e9825fa75ca24ce3e
허예진 <yejin097@naver.com>
2022-05-10 23:27:18 +0900

    Feat: blog 페이지 생성

---
+++ src/pages/blog.tsx
@@ -0,0 +1,52 @@
+import React, { FunctionComponent } from 'react'
+import { graphql } from 'gatsby'
+import { PostListItemType } from 'types/PostItem.types'
+import Layout from 'components/Layout/Layout'
+import PostList from 'components/Post/PostList'
+import Description from 'components/Layout/Description'
+
+type BlogProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostListItemType[]
+    }
+  }
+}
+
+const BlogPage: FunctionComponent<BlogProps> = function ({
+  data: {
+    allMarkdownRemark: { edges }
+  }
+}) {
+ return (
+  <Layout>
+    <Description title="blog" description={`${edges.length}개의 게시글`} />
+    <PostList posts={ edges } />
+  </Layout>
+ )
+}
+
+export const BlogQuery = graphql`
+  {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date
+            summary
+            categories
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default BlogPage
