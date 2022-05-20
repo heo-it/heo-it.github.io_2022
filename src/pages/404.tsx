a785f0932173ebcf3f266a841978ee382316738c
허예진 <yejin@softin.co.kr>
2022-05-20 17:45:08 +0900

    Feat: 404 페이지 생성

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react'
+import Layout from 'components/Layout/Layout'
+
+const NotFoundPage: FunctionComponent = function () {
+
+  return (
+    <Layout>
+      <h3>404 페이지</h3>
+    </Layout>
+  )
+}
+
+export default NotFoundPage
