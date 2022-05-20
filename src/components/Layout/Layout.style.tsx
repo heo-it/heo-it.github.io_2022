652751f1bf2ce8a2323c92903103a9fc642cbb44
허예진 <yejin@softin.co.kr>
2022-05-20 14:30:03 +0900

    Style: 코드 정리

--- src/components/Layout/Layout.style.tsx
+++ src/components/Layout/Layout.style.tsx
@@ -2,19 +2,19 @@
 import styled from '@emotion/styled'
 
 const LayoutWrapper = styled.div`
-min-height: 100vh;
-padding: 5rem 0 0;
-display: flex;
-flex-direction: column;
+  min-height: 100vh;
+  padding: 5rem 0 0;
+  display: flex;
+  flex-direction: column;
 
-& main {
-  margin-top: auto;
-  margin-bottom: auto;
-}
+  & main {
+    margin-top: auto;
+    margin-bottom: auto;
+  }
 
-& footer {
-  margin-top: auto;
-}
+  & footer {
+    margin-top: auto;
+  }
 `
 
 export default LayoutWrapper
